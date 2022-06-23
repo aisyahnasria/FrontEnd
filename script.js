1842ca164540b475cbc4a2a3d6e8eecde44b4289
lailicahyani <202410102013@mail.unej.ac.id>
2022-06-23 13:47:54 +0700

    coba page 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const api_url = "https://paa-product-4.herokuapp.com/product";
+const riview_url = "https://paa-review.herokuapp.com/review/?productId=2";
   
 // Defining async function
 async function getapi(url) {
@@ -31,7 +32,7 @@ function showPage4(data) {
     tab += 
       `
         <div class=" card">
-          <img src="${i.image}" alt="gambar" class="card-img-top">
+          <img src="img/laptop3.png" alt="gambar" class="card-img-top">
               <div class="card-body">
                   <h5 class="card-title">${i.name}</h5>
                   <p class="card-text">Rp ${i.price}</p>
@@ -63,7 +64,7 @@ function showPage5(data) {
     tab += 
       `
         <div class=" card">
-          <img src="${i.image}" alt="gambar" class="card-img-top">
+          <img src="img/laptop1.png" alt="gambar" class="card-img-top">
               <div class="card-body">
                   <h5 class="card-title">${i.name}</h5>
                   <p class="card-text">Rp ${i.price}</p>
@@ -87,3 +88,79 @@ function showPage5(data) {
     // Setting innerHTML as tab variable
     document.getElementById("page5").innerHTML = tab;
 }
+
+
+//  Page 3 
+
+// // Defining async function
+// async function getapi(url) {
+    
+//     // Storing response
+//     const response = await fetch(url);
+    
+//     // Storing data in form of JSON
+//     var data = await response.json();
+//     console.log(data[0]["product_id"]);
+//     if (response) {
+//         hideloader();
+//     }
+//     showPage3(data);
+
+// }
+// // Calling that async function
+// getapi(riview_url);
+  
+// // Function to hide the loader
+// function hideloader() {
+//     document.getElementById('loading').style.display = 'none';
+// }
+// // Function to define innerHTML for HTML table
+// function showPage3(data) {
+//     let tab = ``
+  
+//   for (let i of data) {
+//     tab += 
+//       `
+//       <table border=0>
+//           <tr>
+//               <th><img src=${i.photo}" width="100" height="100"></th>
+//               <td>
+//                   <p>${i.user_id}</p>
+//                   <p>
+//                       <span class="fa fa-star checked"></span>
+//                       <span class="fa fa-star checked"></span>
+//                       <span class="fa fa-star checked"></span>
+//                       <span class="fa fa-star"></span>
+//                       <span class="fa fa-star"></span>
+//                   </p>
+//                   <p>
+//                       <font color="grey"> 20-07-2021 14.30</font>
+//                   </p>
+//               </td>
+//           </tr>
+//           <tr>
+//               <th></th>
+//               <td>
+//                   <p>${i.review}</p>
+//               </td>
+//           </tr>
+//           <tr>
+//               <th></th>
+//               <th>
+//                   <p><button class="button">
+//                           <font color="grey">Kualitas barang bagus</font>
+//                       </button>
+//                       <button class="button">
+//                           <font color="grey">Harga produk sangat baik</font>
+//                       </button>
+//                   </p>
+//               </th>
+//           </tr>
+//       </table>
+//     `;
+  
+    
+//   }
+//     // Setting innerHTML as tab variable
+//     document.getElementById("page3").innerHTML = tab;
+// }
